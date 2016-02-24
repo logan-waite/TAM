f6bdba9ffe646cf21713665e13ace4e607de302d
Logan Waite <lwaite2010@gmail.com>
2016-02-24 12:55:41 -0700

    Begin login

--- index.php
+++ index.php
@@ -1,22 +1,83 @@
-<?php
-    include 'views/includes/header.php';
- ?>
-<header class='main-header'>
-    <h1>PAT</h1>
-</header>
-    <div class='interior'>
-        <div class='sub'>
-            Login
-        </div>
+<html>
+    <head>
+        <link rel='stylesheet' href='resources/bootstrap/css/bootstrap.min.css' type="text/css">
+        <style>
+        header {
+            text-align: center;
+            border-bottom: 1px solid #DADADA;
+            width: 100%;
+            font-family: fantasy, sans-serif;
+            margin-bottom: 5%;
+        }
 
-        <form action="login.php" method='post'>
-            <label for='username'>Username: </label> <input type="text" id="username" name='username'><br>
-            <label for='password'>Password: </label> <input type="password" id="password" name='password'><br><br>
-            <input type="submit" value="Submit">
-        </form>
-        Don't have an account yet? <a href="new_user.html">Click Here</a>
-    </div>
-</body>
+        #page-organizer {
+            width: 1000px;
+            padding-left: 10px;
+        }
+
+        .sub {
+            position: relative;
+            width: 100%;
+            border-bottom: 0;
+            margin: 0px;
+            padding-top: 12px;
+            padding-bottom: 6px;
+            font-size: 24px;
+            margin-bottom: 15px;
+
+        }
+
+        .main-header {
+            position: absolute;
+            top: 50px;
+            left: -50%;
+            margin-left: 50%;
+        }
+        .interior {
+            position: absolute;
+            width: 50%;
+            height: 100%;
+            top: 125px;
+            left: 50%;
+            margin-left: -25%;
+            padding: 10px;
+        }
+
+        .interior form {
+            position: relative;
+            left: 50%;
+            margin-left: -102px;
+            width: 205px;
+            text-align: center;
+            border: 1px solid #DDD;
+            padding: 10px;
+            border-radius: 8px;
+            box-shadow: 3px 3px 3px #AAA;
+        }
+        </style>
+    </head>
+    <body>
+        <header class='main-header'>
+            <h1>PAT</h1>
+        </header>
+        <div class='interior'>
+            <header class='sub'>
+                Login
+            </header>
+
+            <form action="login.php" method='post'>
+                <div class='form-group'>
+                    <label for='username'>Username: </label> <input type="text" id="username" class='form-control' name='username'>
+                </div>
+                <div class='form-group'>
+                    <label for='password'>Password: </label> <input type="password" id="password" class='form-control' name='password'>
+                </div>
+                <input type="submit" value="Submit" class='btn btn-default'> <br><br>
+                Don't have an account yet? <a href="new_user.html">Click Here</a>
+
+            </form>
+        </div>
+    </body>
 </html>
 <?php
     /*
